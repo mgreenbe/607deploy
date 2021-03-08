ffd672926d3c33a550ec111b9abfd989518b5985
Matthew Greenberg <mgreenbe@outlook.com>
2021-03-08 15:29:16 -0700

    telecom

--- app.py
+++ app.py
@@ -16,3 +16,52 @@ def index():
     x = request.args.get("x") or ""
     y = request.args.get("y") or ""
     return render_template("index.html", x=x, y=y)
+
+
+@app.route("/telecom", methods=["GET", "POST"])
+def telecom():
+    print(request.method)
+    if request.method == "POST":
+        form_data = dict(request.form)
+        x = process_form_data(form_data)
+        y = model.predict([x])[0]
+        print(f"x={x}, y={y}")
+        return redirect(url_for("telecom", **form_data, y=y))
+    return render_template("telecom.html", **request.args)
+
+
+def process_form_data(form_data):
+    gender = 1. if form_data["gender"] == "male" else 0.
+    senior = 1. if form_data["senior"] == "yes" else 0.
+    partner = 1. if form_data["partner"] == "yes" else 0.
+    dependents = 1. if form_data["dependents"] == "yes" else 0.
+    paperless = 1. if form_data["paperless"] == "yes" else 0.
+
+    if form_data["payment"] == "electronic_check":
+        payment = [1., 0., 0., 0.]
+    elif form_data["payment"] == "mailed_check":
+        payment = [0., 1., 0., 0.]
+    elif form_data["payment"] == "credit_card":
+        payment = [0., 0., 1., 0.]
+    elif form_data["payment"] == "bank_transfer":
+        payment = [0., 0., 0., 1.]
+    else:
+        raise Exception(f"Unknown payment method: {form_data['payment']}")
+
+    if form_data["contract"] == "month_to_month":
+        contract = [1., 0., 0.]
+    elif form_data["contract"] == "one_year":
+        contract = [0., 1., 0.]
+    elif form_data["contract"] == "two_year":
+        contract = [0., 0., 1.]
+    else:
+        raise Exception(f"Unknown contract type: {form_data['contract']}")
+
+    tenure = float(form_data["tenure"])
+    total = float(form_data["total"])
+    monthly = float(form_data["monthly"])
+
+    x = [gender, senior, partner, dependents, paperless, *payment,
+         *contract, tenure, monthly, total]
+
+    return x
---
+++ model.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from joblib import dump
+
+URL = "https://raw.githubusercontent.com/mgreenbe/telecom/main/telecom_users.csv"
+
+
+def main():
+    df = pd.read_csv(URL, index_col="customerID")
+    df.drop(columns=[df.columns[0]], inplace=True)
+    df = df[df.TotalCharges != " "]
+    y = (df.Churn.values == "Yes").astype(int)
+
+    X = np.zeros((len(df), 15)).astype(float)
+
+    X[:, 0] = df.gender == "Male"
+    X[:, 1] = df.SeniorCitizen
+    X[:, 2] = df.Partner == "Yes"
+    X[:, 3] = df.Dependents == "Yes"
+    X[:, 4] = df.PaperlessBilling == "Yes"
+
+    X[:, 5] = df.PaymentMethod == "Electronic check"
+    X[:, 6] = df.PaymentMethod == "Mailed check"
+    X[:, 7] = df.PaymentMethod == "Credit card (automatic)"
+    X[:, 8] = df.PaymentMethod == "Bank transfer (automatic)"
+
+    X[:, 9] = df.Contract == "Month-to-month"
+    X[:, 10] = df.Contract == "One year"
+    X[:, 11] = df.Contract == "Two year"
+
+    X[:, 12] = df.tenure
+    X[:, 13] = df.MonthlyCharges
+    X[:, 14] = df.TotalCharges.astype(float)
+
+    model = DecisionTreeClassifier()
+    model.fit(X, y)
+    dump(model, "model.joblib")
+
+
+if __name__ == "__main__":
+    main()
