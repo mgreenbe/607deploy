2c8e4bcd4df5942802b1526ff538b4a2d9c5dfd7
Matthew Greenberg <mgreenbe@outlook.com>
2021-03-08 13:24:44 -0700

    xy

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, url_for
 import joblib
 
 app = Flask(__name__)
 model = joblib.load("model.joblib")
 
 
-@app.route('/')
-def hello_world():
-    y_pr = model.predict([range(30), range(1, 31), range(2, 32)])
-    return 'Hello, World!\n' + str(y_pr)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    print(request.method)
+    if request.method == "POST":
+        x = request.form["x"]
+        y = f"{2*float(x):.2f}"
+        print(f"x={x}, y={y}")
+        return redirect(url_for("index", x=x, y=y))
+    x = request.args.get("x") or ""
+    y = request.args.get("y") or ""
+    return render_template("index.html", x=x, y=y)
